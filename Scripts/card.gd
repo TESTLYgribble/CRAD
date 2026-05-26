c27086cee67dfae0ba60879bc5e3c8c409eda618
Kadala <muammedjazaj@gmail.com>
2026-05-26 15:34:57 +0530

    small coloring fix

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -3,8 +3,11 @@ extends Node2D
 const val:Array[String] = ["A","2","3","4","5","6","7","8","9","10","K","Q","J"]
 const cardTypes = ["spd","clvr","dmnd","hrts"]
 
+
 @onready var card_symbol = $whiteCard/Card_symbol
 @onready var card_value = $whiteCard/Card_Value
+@onready var card_color= $whiteCard/Card_Value.material  as ShaderMaterial
+
 
 var rnd_info:Array[int]
 
@@ -17,6 +20,7 @@ func _ready() -> void:
 	randomize()
 	Rnd_Card_Gen()
 	randomCardDetails()
+	checkBlack()
 	print("Hello!")
 
 
@@ -38,3 +42,14 @@ func randomCardDetails() -> void:
 
 	card_value.animation = "Val_Text"
 	card_value.frame = rnd_info[1]
+	
+func checkBlack():
+	if rnd_info[0] in [0, 1] :
+		card_color.set_shader_parameter("isBlack",true)
+
+		
+
+	
+ 
+
+		
