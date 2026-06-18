96566302054e7fbe2bc8512831b03426abfeeef5
Kadala <muammedjazaj@gmail.com>
2026-06-18 18:01:45 +0530

    Added duplicate card deletion and implemented hand array

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,22 +4,41 @@ var cardPos_X: int = 0
 var cardPos_Y: int = 80
 var cardOffset: int = 52
 
-var generated_hand=[]
+var gen_hand: Array = []
 
-# Best practice: Use your exported variable in the inspector, 
-# or fallback to preload if it's empty
 @export var new_Card: PackedScene = preload("res://Scenes/card.tscn")
 
-func card_Spawn():
-	for i in range(5):
+func card_spawn():
+	var Hand: Array = []
+
+	while Hand.size() < 5:
 		var card = new_Card.instantiate()
-		
-		# Set positions before adding to child so it doesn't jump visual frames
-		card.position = Vector2(cardPos_X + (i * cardOffset), cardPos_Y)
-		
 		add_child(card)
 		card.GenerateCard()
+
+		var cardDets = str(card.card_info.num) + str(card.card_info.type)
+
+		# Skip duplicates
+		if Hand.has(cardDets):
+			print("Duplicate generated:", cardDets) #for Debug
+			card.queue_free()
+			continue
+
+		Hand.append(cardDets)
+
+		card.position = Vector2(
+			cardPos_X + ((Hand.size() - 1) * cardOffset),
+			cardPos_Y
+		)
+
 		
+
+	gen_hand.append(Hand)
+
+func debug_info():
+	print("Current Hand:", gen_hand)
+
 func _ready() -> void:
 	randomize()
-	card_Spawn()
+	card_spawn()
+	debug_info()
